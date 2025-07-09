6cf6b9bafc586850a44fd3a9a0c8039b919ac696
Eliza Evans <eve9du@virginia.edu>
2025-07-09 14:01:54 -0400

    Pass info to agent-worker from frontend

---
+++ test/backend/test/frontend/.next/server/vendor-chunks/@livekit+components-styles@1.1.6.js
@@ -0,0 +1,25 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+exports.id = "vendor-chunks/@livekit+components-styles@1.1.6";
+exports.ids = ["vendor-chunks/@livekit+components-styles@1.1.6"];
+exports.modules = {
+
+/***/ "(rsc)/./node_modules/.pnpm/@livekit+components-styles@1.1.6/node_modules/@livekit/components-styles/dist/general/index.css":
+/*!****************************************************************************************************************************!*\
+  !*** ./node_modules/.pnpm/@livekit+components-styles@1.1.6/node_modules/@livekit/components-styles/dist/general/index.css ***!
+  \****************************************************************************************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (\"e0d4a9e27a75\");\nif (false) {}\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKHJzYykvLi9ub2RlX21vZHVsZXMvLnBucG0vQGxpdmVraXQrY29tcG9uZW50cy1zdHlsZXNAMS4xLjYvbm9kZV9tb2R1bGVzL0BsaXZla2l0L2NvbXBvbmVudHMtc3R5bGVzL2Rpc3QvZ2VuZXJhbC9pbmRleC5jc3MiLCJtYXBwaW5ncyI6Ijs7OztBQUFBLGlFQUFlLGNBQWM7QUFDN0IsSUFBSSxLQUFVLEVBQUUsRUFBdUIiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9jcmVhdGUtaGVkcmEtYXZhdGFyLXRlc3QvLi9ub2RlX21vZHVsZXMvLnBucG0vQGxpdmVraXQrY29tcG9uZW50cy1zdHlsZXNAMS4xLjYvbm9kZV9tb2R1bGVzL0BsaXZla2l0L2NvbXBvbmVudHMtc3R5bGVzL2Rpc3QvZ2VuZXJhbC9pbmRleC5jc3M/OGE4OCJdLCJzb3VyY2VzQ29udGVudCI6WyJleHBvcnQgZGVmYXVsdCBcImUwZDRhOWUyN2E3NVwiXG5pZiAobW9kdWxlLmhvdCkgeyBtb2R1bGUuaG90LmFjY2VwdCgpIH1cbiJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///(rsc)/./node_modules/.pnpm/@livekit+components-styles@1.1.6/node_modules/@livekit/components-styles/dist/general/index.css\n");
+
+/***/ })
+
+};
+;
