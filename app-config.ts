ea71b7797395b0ebdfda79a04017cd4b1a922861
Thomas Yuill <thomas.yuill@livekit.io>
2025-11-21 15:48:54 -0500

    update app-config default visualizer

--- app-config.ts
+++ app-config.ts
@@ -35,7 +35,7 @@ export const APP_CONFIG_DEFAULTS: AppConfig = {
   logoDark: '/lk-logo-dark.svg',
   accentDark: '#1fd5f9',
   startButtonText: 'Start call',
-  audioVisualizer: 'grid',
+  audioVisualizer: 'aura',
 
   // for LiveKit Cloud Sandbox
   sandboxId: undefined,
