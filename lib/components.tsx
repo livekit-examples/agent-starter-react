95d11641274f9401d9e27eb3b4c811271533f0fb
Thomas Yuill <thomas.yuill@livekit.io>
2025-12-10 14:31:09 -0500

    fix preview

--- lib/components.tsx
+++ lib/components.tsx
@@ -1,10 +1,19 @@
-import * as fs from 'node:fs';
-import * as path from 'node:path';
-
 export function getComponentNames() {
-  const componentsDir = path.join(process.cwd(), 'components/demos');
-  const componentNames = fs.readdirSync(componentsDir);
-  return componentNames
-    .filter((file) => file.endsWith('.tsx'))
-    .map((file) => file.replace('.tsx', ''));
+  return [
+    ' AgentControlBar',
+    'Alert',
+    'AlertToast',
+    'AudioBarVisualizer',
+    'AudioGridVisualizer',
+    'AudioOscilloscopeVisualizer',
+    'AudioRadialVisualizer',
+    'AudioShaderVisualizer',
+    'Button',
+    'ChatEntry',
+    'ChatIndicator',
+    'Select',
+    'ShimmerText',
+    'Toggle',
+    'TrackControl',
+  ];
 }
